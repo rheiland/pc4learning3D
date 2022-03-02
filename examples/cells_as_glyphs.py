e2000925a6e7b02eebdfb2103fcacf7a83bb931e
Randy Heiland <heiland@indiana.edu>
2022-03-02 13:26:04 -0500

    PBR

--- examples/cells_as_glyphs.py
+++ examples/cells_as_glyphs.py
@@ -5,26 +5,30 @@
 import vtkmodules.vtkInteractionStyle
 # noinspection PyUnresolvedReferences
 import vtkmodules.vtkRenderingOpenGL2
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkCommonCore import vtkPoints, vtkFloatArray
-from vtkmodules.vtkCommonDataModel import vtkPolyData
-from vtkmodules.vtkFiltersCore import vtkGlyph3D
-from vtkmodules.vtkFiltersSources import vtkSphereSource
-from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkPolyDataMapper,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer
-)
+
+from vtk import *
+# from vtkmodules.vtkCommonColor import vtkNamedColors
+# from vtkmodules.vtkCommonCore import vtkPoints, vtkFloatArray
+# from vtkmodules.vtkCommonDataModel import vtkPolyData,vtkUnsignedCharArray
+# from vtkmodules.vtkFiltersCore import vtkGlyph3D
+# from vtkmodules.vtkFiltersSources import vtkSphereSource
+# from vtkmodules.vtkRenderingCore import (
+#     vtkActor,
+#     vtkPolyDataMapper,
+#     vtkRenderWindow,
+#     vtkRenderWindowInteractor,
+#     vtkRenderer
+# )
+
 from pyMCDS_cells import pyMCDS_cells
 from vtk.util import numpy_support
 import numpy as np
+import random
 
 
 def main():
     mcds = pyMCDS_cells('output00000003.xml', '.')  # 23123 cells
-    # mcds = pyMCDS_cells('output00000003.xml', '.')  
+    # mcds = pyMCDS_cells('output00000000.xml', '.')  
     print('time=', mcds.get_time())
 
     print(mcds.data['discrete_cells'].keys())
@@ -55,6 +59,8 @@ def main():
     print("zmax = ",zmax)
 
     cell_type = mcds.data['discrete_cells']['cell_type']
+    print(type(cell_type))
+    print(cell_type)
     unique_cell_type = np.unique(cell_type)
     print("\nunique_cell_type = ",unique_cell_type )
 
@@ -73,44 +79,83 @@ def main():
         z= mcds.data['discrete_cells']['position_z'][idx]
         id = mcds.data['discrete_cells']['cell_type'][idx]
         points.InsertNextPoint(x, y, z)
-        cellVolume.InsertNextValue(30.0)
+        # cellVolume.InsertNextValue(30.0)
         cellID.InsertNextValue(id)
 
     polydata = vtkPolyData()
     polydata.SetPoints(points)
-    polydata.GetPointData().SetScalars(cellVolume)
-    # polydata.GetPointData().SetScalars(cellID)
+    # polydata.GetPointData().SetScalars(cellVolume)
+    polydata.GetPointData().SetScalars(cellID)
+
+    cellID_color_dict = {}
+    # for idx in range(ncells):
+    random.seed(42)
+    for utype in unique_cell_type:
+        # colors.InsertTuple3(0, randint(0,255), randint(0,255), randint(0,255)) # reddish
+        cellID_color_dict[utype] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+    cellID_color_dict[0.0]=[255,255,0]  # yellow basement membrane
+    print("color dict=",cellID_color_dict)
+
+    colors = vtkUnsignedCharArray()
+    colors.SetNumberOfComponents(3)
+    colors.SetNumberOfTuples(polydata.GetNumberOfPoints())  # ncells
+    for idx in range(ncells):
+    # for idx in range(len(unique_cell_type)):
+        # colors.InsertTuple3(idx, randint(0,255), randint(0,255), randint(0,255)) 
+        # if idx < 5:
+            # print(idx,cellID_color_dict[cell_type[idx]])
+        colors.InsertTuple3(idx, cellID_color_dict[cell_type[idx]][0], cellID_color_dict[cell_type[idx]][1], cellID_color_dict[cell_type[idx]][2])
+
+    polydata.GetPointData().SetScalars(colors)
 
     sphereSource = vtkSphereSource()
-    nres = 12
+    nres = 20
     sphereSource.SetPhiResolution(nres)
     sphereSource.SetThetaResolution(nres)
+    sphereSource.SetRadius(0.1)
 
-    glyph3D = vtkGlyph3D()
-    glyph3D.SetSourceConnection(sphereSource.GetOutputPort())
-    glyph3D.SetInputData(polydata)
-    glyph3D.SetColorModeToColorByScalar()
-    glyph3D.ScalingOn()
-    glyph3D.Update()
+    glyph = vtkGlyph3D()
+    glyph.SetSourceConnection(sphereSource.GetOutputPort())
+    glyph.SetInputData(polydata)
+    glyph.SetColorModeToColorByScalar()
+    # glyph.SetScaleModeToDataScalingOff()
+    # glyph.SetScaleModeToDataScalingOn()
+    # glyph.ScalingOn()
+    # glyph.SetScaleFactor(2)  # overall (multiplicative) scaling factor
+    glyph.Update()
+
 
     # Visualize
     mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(glyph3D.GetOutputPort())
+    mapper.SetInputConnection(glyph.GetOutputPort())
 
     actor = vtkActor()
     actor.SetMapper(mapper)
+    actor.GetProperty().SetInterpolationToPBR()
     # actor.GetProperty().SetColor(colors.GetColor3d('Salmon'))
+    print("-- actor defaults:")
+    print("-- diffuse:",actor.GetProperty().GetDiffuse())
+    print("-- specular:",actor.GetProperty().GetSpecular())
+    print("-- roughness:",actor.GetProperty().GetCoatRoughness ())
+    # actor.GetProperty().SetDiffuse(1.0)
+    # actor.GetProperty().SetSpecular(0.2)
+    actor.GetProperty().SetCoatRoughness (0.5)
+    actor.GetProperty().SetCoatRoughness (0.2)
+    actor.GetProperty().SetCoatRoughness (1.0)
 
     renderer = vtkRenderer()
+    amval = 1.0  # default
+    renderer.SetAmbient(amval, amval, amval)
+
     renderWindow = vtkRenderWindow()
     renderWindow.SetPosition(100,100)
-    renderWindow.SetSize(800,800)
+    renderWindow.SetSize(1400,1200)
     renderWindow.AddRenderer(renderer)
     renderWindowInteractor = vtkRenderWindowInteractor()
     renderWindowInteractor.SetRenderWindow(renderWindow)
 
     renderer.AddActor(actor)
-    renderer.SetBackground(colors.GetColor3d('SlateGray'))  # Background Slate Gray
+    # renderer.SetBackground(colors.GetColor3d('SlateGray'))  # Background Slate Gray
 
     renderWindow.SetWindowName('PhysiCell model')
     renderWindow.Render()
